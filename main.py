70b231d3cdd1e3c388c5efa0666598234826c024
Jimmy <jaimedelafuente25@outlook.es>
2023-09-13 14:22:21 +0200

    Add requirements

--- main.py
+++ main.py
@@ -2,8 +2,44 @@ import keyboard
 from game import *
 import time
 
+key_mapping = {
+    'flecha abajo': Movement.DOWN,
+    'down': Movement.DOWN,
+    's': Movement.DOWN,
+    'flecha derecha': Movement.RIGHT,
+    'right': Movement.RIGHT,
+    'd': Movement.RIGHT,
+    'flecha izquierda': Movement.LEFT,
+    'left': Movement.LEFT,
+    'a': Movement.LEFT,
+    'space': Movement.ROTATE
+}
+
 
 def main():
+    '''## Summary
+    The `main` function is the entry point of the program. It initializes the game board, prints the initial board, and then enters a loop to handle user input and update the game state until the game is over or the user presses the 'esc' key.
+
+    ## Example Usage
+    ```python
+    main()
+    ```
+    ## Code Analysis
+    ### Inputs
+    - None
+    ### Flow
+    1. Initialize the game board.
+    2. Print the initial board.
+    3. Set the initial values for the `key` and `game_over` variables.
+    4. Enter a loop that continues until the user presses the 'esc' key or the game is over.
+    5. Read the user's keyboard input.
+    6. If the input is a key press event, check if it is a valid key for moving the game piece.
+    7. If it is a valid key, move the game piece accordingly on the game board.
+    8. Check if the game is over by iterating through the first row of the game board and checking if any cell is not empty.
+    9. If the game is over, set the `game_over` variable to True to exit the loop.
+    ### Outputs
+    - None
+    '''
     # Initializing the game
     board = Board()
 
@@ -11,28 +47,19 @@ def main():
     board.print_board()
 
     key = None
-    end = False
+    game_over = False
     # The Key 'escape' exit the game
-    while (key != 'esc' and not end):
+    while (key != 'esc' and not game_over):
         listener = keyboard.read_event()
 
         if listener.event_type == keyboard.KEY_DOWN:
             key = listener.name
-            if key == 'flecha abajo' or key == 'down' or key == 's':
-                board.move_piece(Movement.DOWN)
-
-            elif key == 'flecha derecha' or key == 'right' or key == 'd':
-                board.move_piece(Movement.RIGHT)
-
-            elif key == 'flecha izquierda' or key == 'left' or key == 'a':
-                board.move_piece(Movement.LEFT)
-
-            elif key == 'space':
-                board.move_piece(Movement.ROTATE)
+            if key in key_mapping:
+                board.move_piece(key_mapping[key])
 
         for cell in board.boxes[0]:
             if cell != "⬜":
-                end = True
+                game_over = True
                 break
 
     print("Game Over")
