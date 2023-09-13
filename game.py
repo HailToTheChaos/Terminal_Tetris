9fd071871475234e30b9d88532168e32d9775b70
Jimmy <jaimedelafuente25@outlook.es>
2023-09-13 12:10:27 +0200

    Add rotation control

--- game.py
+++ game.py
@@ -1,12 +1,20 @@
+from game import _JBlock, _IBlock, _LBlock, _SquareBlock, _TBlock, _ZBlock, _SBlock
+from typing import Union
 import random
 import os
 from enum import Enum
 import copy
 
-COLOURS = ('🟦', '🟥', '🟪', '🟩')
+
+COLOURS = ('🟦', '🟥', '🟪', '🟩', '🟧', '🟨')
 
 
 class Movement(Enum):
+    ''' 
+    MOVEMENT
+    ---- 
+    The class `Movement` is an enumeration that represents different types of movements.
+    '''
     DOWN = 0
     RIGHT = 1
     LEFT = 2
@@ -14,6 +22,12 @@ class Movement(Enum):
 
 
 class Board():
+    ''' 
+    BOARD
+    ----
+    The `Board` class represents a game board and manages the movement and placement of pieces on the
+    board.'''
+
     BOARD = [["⬜"]*10 for _ in range(10)]
 
     def __init__(self):
@@ -21,9 +35,8 @@ class Board():
         self.piece = Piece()
 
     def print_board(self) -> None:
-        # Esto es para borrar la pantalla en Windows, puedes ajustarlo para tu sistema operativo.
         os.system('cls')
-
+        print('\033[91m ESC to exit')
         # Hacer una copia temporal del tablero
         temp_board = copy.deepcopy(self.boxes)
 
@@ -45,7 +58,7 @@ class Board():
         if movement == Movement.ROTATE:
             self.piece.rotation_state = 0 if self.piece.rotation_state == 3 else self.piece.rotation_state+1
 
-        if self._movement_control(movement):
+        if self._is_valid_move(movement):
             for box_index, box in enumerate(self.piece.shape.position):
                 match movement:
                     case Movement.DOWN:
@@ -57,10 +70,11 @@ class Board():
                     case Movement.ROTATE:
                         box[0] += self.piece.shape.rotation[self.piece.rotation_state][box_index][0]
                         box[1] += self.piece.shape.rotation[self.piece.rotation_state][box_index][1]
+                        self._rotation_control(box)
 
         self.print_board()
 
-    def _movement_control(self, movement: Movement) -> bool:
+    def _is_valid_move(self, movement: Movement) -> bool:
         for box in self.piece.shape.position:
             match movement:
                 case Movement.DOWN:
@@ -75,6 +89,19 @@ class Board():
                         return False
         return True
 
+    def _rotation_control(self, box) -> None:
+        if box[0] < 0:
+            for item in self.piece.shape.position:
+                item[0] += 1
+
+        if box[0] > 9:
+            for item in self.piece.shape.position:
+                item[0] -= 1
+
+        if box[1] > 9:
+            for item in self.piece.shape.position:
+                item[1] -= 1
+
 
 class Piece:
     def __init__(self):
@@ -82,22 +109,39 @@ class Piece:
         self.shape = self._random_shape()
         self.rotation_state = 0
 
-    def _random_shape(self):
-        # Lista de clases de piezas disponibles
-        pieces = [_JBlock, _IBlock, _LBlock]
+    def _random_shape(self) -> Union[_JBlock, _IBlock, _LBlock, _SquareBlock, _TBlock, _ZBlock, _SBlock]:
+        '''The function randomly selects and returns an instance of a shape class from a list of available
+        shapes.
 
-        # Selecciona una clase de pieza aleatoria
+        Returns
+        -------
+            an instance of a randomly selected shape class.
+
+        '''
+        # List of available shapes
+        pieces = [_JBlock, _IBlock, _LBlock,
+                  _SquareBlock, _TBlock, _ZBlock, _SBlock]
+
+        # Selecting random shape class
         selected_piece = random.choice(pieces)
 
+        # Returning and instantiating random shape
         return selected_piece()
 
     def change_piece(self):
+        # Selecting random colour
         self.colour = COLOURS[random.randint(0, len(COLOURS)-1)]
+        # Selecting random shape
         self.shape = self._random_shape()
+        # Reseting rotation state
         self.rotation_state = 0
 
 
 class _JBlock:
+    '''🟦⬜⬜\n
+        🟦🟦🟦\n
+        ⬜⬜⬜'''
+
     def __init__(self):
         self.position = [[0, 0], [0, 1], [1, 1], [2, 1]]
         self.rotation = [[(0, -2), (-1, -1), (0, 0), (1, 1)],
@@ -107,6 +151,8 @@ class _JBlock:
 
 
 class _IBlock:
+    '''🟦🟦🟦🟦'''
+
     def __init__(self):
         self.position = [[0, 1], [1, 1], [2, 1], [3, 1]]
         self.rotation = [[(-1, -2), (0, -1), (1, 0), (2, 1)],
@@ -116,9 +162,55 @@ class _IBlock:
 
 
 class _LBlock:
+    '''⬜⬜🟦 \n
+    🟦🟦🟦\n
+    ⬜⬜⬜'''
+
     def __init__(self):
         self.position = [[2, 0], [2, 1], [1, 1], [0, 1]]
-        self.rotation = [[[2, 0], [1, 1], (0, 0), (-1, -1)],
+        self.rotation = [[(2, 0), (1, 1), (0, 0), (-1, -1)],
                          [(0, 2), (-1, 1), (0, 0), (1, -1)],
                          [(-2, 0), (-1, -1), (0, 0), (1, 1)],
                          [(0, -2), (1, -1), (0, 0), (-1, 1)]]
+
+
+class _SquareBlock:
+    '''🟦🟦\n
+    🟦🟦'''
+
+    def __init__(self):
+        self.position = [[1, 0], [2, 0], [1, 1], [2, 1]]
+        self.rotation = [[(0, 0), (0, 0), (0, 0), (0, 0)],
+                         [(0, 0), (0, 0), (0, 0), (0, 0)],
+                         [(0, 0), (0, 0), (0, 0), (0, 0)],
+                         [(0, 0), (0, 0), (0, 0), (0, 0)]]
+
+
+class _TBlock:
+    '''🟦🟦🟦\n
+    ⬜🟦⬜'''
+
+    def __init__(self):
+        self.position = [[1, 0], [0, 1], [1, 1], [2, 1]]
+        self.rotation = [[(1, -1), (-1, -1), (0, 0), (1, 1)],
+                         [(1, 1), (1, -1), (0, 0), (-1, 1)],
+                         [(-1, 1), (1, 1), (0, 0), (-1, -1)],
+                         [(-1, -1), (-1, 1), (0, 0), (1, -1)]]
+
+
+class _ZBlock:
+    def __init__(self):
+        self.position = [[0, 0], [1, 0], [1, 1], [2, 1]]
+        self.rotation = [[(0, -2), (1, -1), (0, 0), (1, 1)],
+                         [(2, 0), (1, 1), (0, 0), (-1, 1)],
+                         [(0, 2), (-1, 1), (0, 0), (-1, -1)],
+                         [(-2, 0), (-1, -1), (0, 0), (1, -1)]]
+
+
+class _SBlock:
+    def __init__(self):
+        self.position = [[2, 0], [1, 0], [1, 1], [0, 1]]
+        self.rotation = [[(2, 0), (1, -1), (0, 0), (-1, -1)],
+                         [(0, 2), (1, 1), (0, 0), (1, -1)],
+                         [(-2, 0), (-1, 1), (0, 0), (1, 1)],
+                         [(0, -2), (-1, -1), (0, 0), (-1, 1)]]
